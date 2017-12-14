5122434baa40cac5d08bacc695fd2125ed4774d8
Ryan <ryan.culpepper@gmail.com>
2017-12-13 21:00:06 -0600

    More layout improvements

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :default_members_collection, :set_layout_variables
+  before_action :default_members_collection, :set_city
   before_action :set_news_items, only: [:index]
 
   def index
     @title = 'City of Meridian'
     @events = Event.limit(4)
     @public_notices = public_notices
+    @latest_public_notice = @public_notices.first if @public_notices.present?
   end
 
   def edc
@@ -44,12 +45,13 @@ class ApplicationController < ActionController::Base
     @default_members_collection = Organization.default_members_collection_name
   end
 
-  def set_layout_variables
+  def set_city
     @city = Organization.find_by(name: 'City')
   end
 
   def set_news_items
     @news_items = NewsItem.where('created_at > ?', 2.weeks.ago)
+    
   end
 
 end
